6c1eec77e8036556ec2ef4f9bfec343f7e51f194
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-18 22:46:04 -0300

    feat: change router handler to mux

--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/leodev0/go-rest-api/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
