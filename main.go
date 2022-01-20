882904c230f840460c82bef33a30b8962d5e59f5
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-20 20:54:39 -0300

    feat: setup main file to call db connection and routes

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/leodev0/go-rest-api/database"
 	"github.com/leodev0/go-rest-api/routes"
 )
 
 func main() {
+	database.ConexaoComBancoDeDados()
+
 	fmt.Println("Iniciando servidor")
 	routes.HandleRequest()
 }
